ba09b7f8a7d5facc967da581d38853eacdc4c2e3
NegrosG <negrosmartins@gmail.com>
2023-11-06 17:24:09 -0800

    More returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    lenght = len(sentence)
-    if lenght > 0:
-        f_char = sentence[0]
-    else:
-        None
-    tuple_s = lenght, f_char
-    return(tuple_s)
+    return(len(sentence), sentence[0] if len(sentence) > 0 else None)
