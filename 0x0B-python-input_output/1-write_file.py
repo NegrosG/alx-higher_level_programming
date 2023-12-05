c41c8d2b788621df38a0dc27e4eeef74aa6b1931
NegrosG <negrosmartins@gmail.com>
2023-12-05 13:45:49 -0800

    Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Defining the write_fie function'''
+
+
+def write_file(filename="", text=""):
+    '''read file with utf-8'''
+    with open(filename,"w", encoding='utf-8') as files:
+        return files.write(text)
