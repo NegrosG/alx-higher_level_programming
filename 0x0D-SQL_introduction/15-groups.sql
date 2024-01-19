f5036e97a9708526557258df8267019066352b32
NegrosG <negrosmartins@gmail.com>
2024-01-18 18:07:17 -0800

    all mandatories

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- List the number of record of the same value in second_table
 SELECT `score`, COUNT(*) AS  number
 FROM `second_table`
-GROUP BY `score`;
-ORDER BY `number` DESC
+GROUP BY `score`
+ORDER BY `number` DESC;
