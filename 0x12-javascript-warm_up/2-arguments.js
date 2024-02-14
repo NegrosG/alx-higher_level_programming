8a312c2f13f60203108811495b0a1b98e0fe1e37
Onifade Martins <negrosmartins@gmail.com>
2024-02-14 08:28:54 -0800

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (process.argv.lenght === 2) {
+if (process.argv.lenght !== 2) {
   console.log('No argument');
 } else if (process.argv.lenght === 3) {
   console.log('Argument found');
