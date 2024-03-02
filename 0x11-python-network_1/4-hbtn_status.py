74e6b7315ee5ce02a6458ebe49635266a31c2216
NegrosG <negrosmartins@gmail.com>
2024-03-01 17:26:30 -0800

    my status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,3 +6,5 @@ import requests
 if __name__ == "__main__":
     req = requests.get("https://alx-intranet.hbtn.io/status")
     print("body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
